6851c092cd7bacec9283cda3f8a96d54c44b2aa2
IIIAuPMa <kalash1605@mail.ru>
2025-05-04 18:54:31 +0300

    Add files via upload

---
+++ Config/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins.Configuration;
+using UnityEngine;
+
+namespace Configuration 
+{
+    internal class Config : Properties
+    {
+        
+        public Color HCZColor = Color.cyan;
+
+        public readonly string MessageToAllPlayers = "Запущена последовательность блокировки\n Зоны Тяжелого Содержания";
+
+        public readonly string CassieWords = "pitch_00.15 .g4 .g4 .g4 pitch_00.95 . Heavy Containment Zone locked down sequence engaged . Heavy Containment Zone will lock down in . tminus 3 minutes . all personnel must evacuate from heavy containment zone immediataly yield_50 2 minutes yield_29 90 seconds yield_29 60 seconds yield_9 50 yield_9 40 yield_9 30 seconds all heavy doors and checkpoint doors locked down yield_7 20 yield_6 10 seconds . 9 . 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 . pitch_00.15 .g4 . pitch_00.25 .g3 . pitch_00.95 Heavy Containment Zone locked down sequence begun . . . . .  . . . . . pitch_00.05 .g5 pitch_01.00 .g4 .g4 .g4 .g4 .g3 cassiesystem detected locked down sequence in Heavy Containment Zone . all remaining personnel in Heavy Containment Zone please wait any repair personnel for repaired Heavy Containment Doors pitch_00.97 . Secure . pitch_00.94 Contain . pitch_00.92 Protect";
+
+    }
+
+}
---
+++ Sequence/Sequence.cs
@@ -0,0 +1,87 @@
+﻿using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+using MapGeneration;
+using UnityEngine;
+using Unity;
+using MEC;
+using HCZLock;
+using MusicPlayer;
+
+
+namespace HCZLock
+{
+    internal class HeavyCZLock : Plugin<Configuration.Config>
+    {
+        Configuration.Config config { get; set; }
+
+        public Plugin plugin;
+        public override string Name => "HCZSequence";
+
+        public override string Description => "Lock Down Sequence in HCZ";
+
+        public override string Author => "IIIAuPMa";
+
+        public override System.Version Version { get; }
+
+        public override System.Version RequiredApiVersion { get; }
+
+        public bool HCZIsLocked = false;
+
+        public override void Disable()
+        {
+            LabApi.Events.Handlers.ServerEvents.LczDecontaminationStarted -= Sequence;            
+        }
+        public override void Enable()
+        {
+            LabApi.Events.Handlers.ServerEvents.LczDecontaminationStarted += Sequence;
+        }
+
+        public void Sequence()
+        {
+
+            if (!Cassie.IsSpeaking && HCZIsLocked == false)
+
+            {
+
+            Map.TurnOnLights(FacilityZone.HeavyContainment);
+
+            foreach (Door door in Door.Get(FacilityZone.HeavyContainment))
+                {
+
+                    door.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Lockdown2176, false);
+
+                    door.IsLocked = false;
+
+                }
+
+                HCZSystems.HeavyContainmentZone heavyContainmentZone = new HCZSystems.HeavyContainmentZone();
+
+                heavyContainmentZone.HCZAnnouncement();
+
+                heavyContainmentZone.Message();
+
+                heavyContainmentZone.SetLightColor();
+
+                Timing.CallDelayed(167, () => heavyContainmentZone.HCZDoorsAndCheckpointsLockedDown());
+
+                Timing.CallDelayed(197, () => heavyContainmentZone.TurnOffHCZLights());
+
+                HCZIsLocked = true;
+
+                if (HCZIsLocked == true)
+                {
+
+                   //LockDown Sequence Begun Now
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
+
+

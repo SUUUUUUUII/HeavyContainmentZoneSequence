feb1cdf13365b41a5e9111d8f8bfe071a29c8bb8
IIIAuPMa <kalash1605@mail.ru>
2025-05-05 15:59:39 +0300

    Add files via upload

---
+++ Sequence.cs
@@ -0,0 +1,84 @@
+﻿using LabApi.Features.Wrappers;
+using LabApi.Loader.Features.Plugins;
+using MapGeneration;
+using UnityEngine;
+using Unity;
+using MEC;
+using HCZLock;
+
+
+namespace HCZLock
+{
+    internal class HeavyCZLock : Plugin<Configuration.Config>
+    {
+        Configuration.Config config { get; set; }
+
+        public Plugin plugin;
+        public override string Name => "HCZSequence";
+
+        public override string Description => "Lock Down Sequence in HCZ";
+
+        public override string Author => "IIIAuPMa";
+
+        public override System.Version Version { get; }
+
+        public override System.Version RequiredApiVersion { get; }
+
+        public bool HCZIsLocked = false;
+
+        public override void Disable()
+        {
+            
+        }
+        public override void Enable()
+        {
+            
+        }
+
+        public void Sequence()
+        {
+
+            if (!Cassie.IsSpeaking)
+
+            {
+
+                Map.TurnOnLights(FacilityZone.HeavyContainment);
+
+                foreach (Door door in Door.Get(FacilityZone.HeavyContainment))
+                {
+
+                    door.Lock(Interactables.Interobjects.DoorUtils.DoorLockReason.Lockdown2176, false);
+
+                    door.IsLocked = false;
+
+                }
+
+                HCZSystems.HeavyContainmentZone heavyContainmentZone = new HCZSystems.HeavyContainmentZone();
+
+                heavyContainmentZone.HCZAnnouncement();
+
+                heavyContainmentZone.Message();
+
+                heavyContainmentZone.SetLightColor(Color.cyan);
+
+                Timing.CallDelayed(167, () => heavyContainmentZone.HCZDoorsAndCheckpointsLockedDown());
+
+                Timing.CallDelayed(197, () => heavyContainmentZone.TurnOffHCZLights());
+
+                HCZIsLocked = true;
+
+                if (HCZIsLocked == true)
+                {
+
+                    //LockDown Sequence Begun Now
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
---
+++ SequenceCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSystem;
+using HCZLock;
+using HCZSystems;
+using LabApi.Features.Wrappers;
+using MEC;
+using Steamworks.Data;
+
+namespace HCZSequence
+{
+
+[CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class SequenceCommand : ICommand
+    {
+        
+
+        public bool SequenceIsStarted = false;
+
+        HCZSystems.HeavyContainmentZone properties = new HeavyContainmentZone();
+
+        HCZLock.HeavyCZLock heavyCZ = new HCZLock.HeavyCZLock();
+        public string Command => "forceHCZSequence";
+
+        public string[] Aliases => new [] {"hczsequence"};
+
+        public string Description => "Engage locked down sequence in Heavy Containment Zone";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+
+            if (!Round.IsRoundStarted)
+            {
+
+                response = "Round must be started for use this command";
+
+                return false;
+
+            }
+
+
+            response = "Done! H.C.Z Locked Down Sequence engaged.";
+
+            heavyCZ.Sequence();
+
+            SequenceIsStarted = true;
+
+            if (Cassie.IsSpeaking && SequenceIsStarted == true && !Round.IsRoundEnded)
+            {
+
+                response = "H.C.Z Sequence already engaged.";
+
+                return false;
+            }
+
+            if (Round.IsRoundEnded)
+            {
+
+                properties.SetLightColor(UnityEngine.Color.clear);
+
+                properties.TurnOnHCZLights();
+
+                properties.UnlockHCZDoors();
+
+                SequenceIsStarted = false;
+
+                return false;
+            }
+
+            return true;
+        }
+        
+    }
+}
